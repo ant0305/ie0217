34a0c35b93343d1b5733ec4c0ccd0f097637300f
Anthony Calvo <anthonycalvo50@gmail.com>
2024-01-10 11:06:39 -0600

    Se modifica adivinanza.cpp y adivinanza.hpp terminando por completo la funcion basica del problema

--- TAREA_UNO/src/adivinanza.cpp
+++ TAREA_UNO/src/adivinanza.cpp
@@ -1,25 +1,24 @@
 #include "adivinanza.hpp"
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 
 void JuegoAdivinanza::iniciarMenu() {
-int opcion;
-do {
+    int opcion;
+    do {
     cout << "Bienvenido al juego de adivinanza:" << endl;
     cout << "1. Jugar adivinanza con indicaciones exactas" << endl;
     cout << "2. Jugar adivinanza con indicaciones 'frio/caliente'" << endl;
     cout << "3. Salir" << endl;
     cout << "Ingrese su opcion: ";
     cin >> opcion;
+    modo = opcion;
 
     switch (opcion) {
         case 1:
-            jugarAdivinanza();
-            break;
         case 2:
             jugarAdivinanza();
             break;
@@ -49,6 +48,42 @@ void JuegoAdivinanza::jugarAdivinanza() {
     int intentosMaximos = (fin - inicio + 1) / 3;
 
 
+    int intentos = 0;
+    int guess;
+    bool adivinado = false;
 
+while (intentos < intentosMaximos && !adivinado) {
+cout << "Intento " << intentos + 1 << ": Ingrese su adivinanza: ";
+cin >> guess;
 
+if (guess == numeroSecreto) {
+    cout << "Has adivinado el numero secreto." << endl;
+    adivinado = true;
+} else {
+    if (modo == 1) {
+        cout << "Incorrecto. El numero secreto es ";
+        if (guess < numeroSecreto) {
+                cout << "mayor." << endl;
+        } else {
+                cout << "menor." << endl;
+        }
+    } else if (modo == 2) {
+            int diferencia = abs(numeroSecreto - guess);
+        if (diferencia == 1) {
+                cout << "Hirviendo! Estas muy cerca." << endl;
+        } else if (diferencia <= 5) {
+                cout << "Caliente. Estas cerca." << endl;
+        } else if (diferencia <= 10) {
+                cout << "Frio. Estas lejos." << endl;
+        } else {
+                cout << "Congelado. Estas muy lejos." << endl;
+        }
+    }
+    intentos++;
+}
+}
+
+if (!adivinado) {
+    cout << "Has agotado tus intentos. El numero secreto era " << numeroSecreto << "." << endl;
+}
 }
--- TAREA_UNO/src/adivinanza.hpp
+++ TAREA_UNO/src/adivinanza.hpp
@@ -5,8 +5,10 @@ class JuegoAdivinanza {
 public:
     void iniciarMenu();
 
-
     void jugarAdivinanza();
+
+private:
+    int modo;
 
 };
 
